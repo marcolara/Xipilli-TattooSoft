6fec3d76ef0c0ad83a23f0728517f4b5ec585d74
Marco Lara <marco@marcolara.com>
2017-08-28 20:18:45 -0700

    Only add roles for now

--- tattoosoft.business/src/main/java/com/tattoosoft/business/service/UserDetailsServiceImpl.java
+++ tattoosoft.business/src/main/java/com/tattoosoft/business/service/UserDetailsServiceImpl.java
@@ -15,10 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tattoosoft.business.model.BusinessUser;
-import com.tattoosoft.persistence.dao.PermissionDAO;
 import com.tattoosoft.persistence.dao.RoleDAO;
 import com.tattoosoft.persistence.dao.UserDAO;
-import com.tattoosoft.persistence.model.Permission;
 import com.tattoosoft.persistence.model.Role;
 import com.tattoosoft.persistence.model.User;
 
@@ -32,9 +30,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private RoleDAO roleDAO;
 
-    @Autowired
-    private PermissionDAO permissionDAO;
-    
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userDAO.findUniqueByProperty(UserDAO.EMAIL_ADDRESS, username.toUpperCase());
@@ -42,14 +37,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("Username not found.");
         }
         List<Role> roles = roleDAO.findByUsername(user.getEmailAddress());
-        List<Permission> permissions = permissionDAO.findByUsername(user.getEmailAddress());
         Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-        if (permissions != null && roles != null) {
+        if (roles != null) {
             for (Role role : roles) {
                 authorities.add(new SimpleGrantedAuthority(role.getDataKey()));
-            }
-            for (Permission permission : permissions) {
-                authorities.add(new SimpleGrantedAuthority(permission.getDataKey()));
             }
         }
         return new BusinessUser(user, authorities);
